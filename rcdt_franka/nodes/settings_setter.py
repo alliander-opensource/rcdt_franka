dac41cb4d01d72e693bf6f9264d2cdc5a010c93c
Jelmer de Wolde <jelmer.de.wolde@alliander.com>
2024-09-27 11:36:24 +0000

    Let settings_setter only spin at start.
    Signed-off-by: Jelmer de Wolde <jelmer.de.wolde@alliander.com>

--- rcdt_franka/nodes/settings_setter.py
+++ rcdt_franka/nodes/settings_setter.py
@@ -7,38 +7,39 @@ import rclpy
 from rclpy.node import Node
 from franka_msgs.srv import SetForceTorqueCollisionBehavior
 
+MAX_TORQUES = [100.0, 100.0, 100.0, 80.0, 80.0, 40.0, 40.0]
+MAX_FORCES = [100.0, 100.0, 100.0, 30.0, 30.0, 30.0]
+
 
 class SettingsSetter(Node):
     def __init__(self):
         super().__init__("settings_setter")
-        self.set_thresholds()
-
-    def set_thresholds(self) -> None:
-        client = self.create_client(
+        self.client = self.create_client(
             SetForceTorqueCollisionBehavior,
             "/service_server/set_force_torque_collision_behavior",
         )
-        request = SetForceTorqueCollisionBehavior.Request()
-        percentage = 1.0
-        torques = [100.0, 100.0, 100.0, 80.0, 80.0, 40.0, 40.0]
-        forces = [100.0, 100.0, 100.0, 30.0, 30.0, 30.0]
-        request.upper_torque_thresholds_nominal = [
-            percentage * torque for torque in torques
-        ]
-        request.upper_force_thresholds_nominal = [
-            percentage * force for force in forces
-        ]
-        while not client.wait_for_service(timeout_sec=1.0):
+        while not self.client.wait_for_service(timeout_sec=1.0):
             self.get_logger().info("service not available, waiting again...")
-        client.call_async(request)
+        self.request = SetForceTorqueCollisionBehavior.Request()
+
+    def set_thresholds(self) -> SetForceTorqueCollisionBehavior.Response:
+        self.request.upper_torque_thresholds_nominal = MAX_TORQUES
+        self.request.upper_force_thresholds_nominal = MAX_FORCES
+        future = self.client.call_async(self.request)
+        rclpy.spin_until_future_complete(self, future)
+        return future.result()
 
 
 def main(args: str = None) -> None:
     rclpy.init(args=args)
-
     settings_setter = SettingsSetter()
-
-    rclpy.spin(settings_setter)
+    response = settings_setter.set_thresholds()
+    if response.success:
+        settings_setter.get_logger().info("Thresholds set successfully.")
+    else:
+        settings_setter.get_logger().warn("Setting thresholds failed.")
+    settings_setter.destroy_node()
+    rclpy.shutdown()
 
 
 if __name__ == "__main__":
