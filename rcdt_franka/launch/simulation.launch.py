904110ac4506889d3f5ed51e193df45cc5619aa3
Jelmer de Wolde <jelmer.de.wolde@alliander.com>
2024-09-30 15:08:25 +0000

    refactor_launch_files
    Signed-off-by: Jelmer de Wolde <jelmer.de.wolde@alliander.com>

--- rcdt_franka/launch/simulation.launch.py
+++ rcdt_franka/launch/simulation.launch.py
@@ -8,25 +8,8 @@ from launch_ros.actions import Node
 
 from rcdt_utilities.launch_utils import get_file_path
 
-gazebo = IncludeLaunchDescription(
-    get_file_path("ros_gz_sim", ["launch"], "gz_sim.launch.py"),
-    launch_arguments={
-        "gz_args": "empty.sdf -r",
-    }.items(),
-)
-
-spawn_robot = Node(
-    package="ros_gz_sim",
-    executable="create",
-    arguments=["-topic", "/robot_description"],
-    output="screen",
-)
-
-sync_clock = Node(
-    package="ros_gz_bridge",
-    executable="parameter_bridge",
-    name="sync_clock",
-    arguments=["/clock@rosgraph_msgs/msg/Clock[gz.msgs.Clock"],
+gazebo_robot = IncludeLaunchDescription(
+    get_file_path("rcdt_utilities", ["launch"], "gazebo_robot.launch.py")
 )
 
 fr3_gripper = Node(
@@ -48,9 +31,7 @@ gripper_action_controller = Node(
 def generate_launch_description() -> LaunchDescription:
     return LaunchDescription(
         [
-            gazebo,
-            spawn_robot,
-            sync_clock,
+            gazebo_robot,
             fr3_gripper,
             gripper_action_controller,
         ]
